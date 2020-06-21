703b18ea9f1585b863328b7d0830f956e3bb214a
Bruno Santos <brunog86@gmail.com>
2020-06-20 22:42:28 -0300

    Init commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,32 @@
+import model.CourseIdea;
+import model.CourseIdeaDAO;
+import model.SimpleCourseIdeaDAO;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        staticFileLocation("/public");
+        CourseIdeaDAO dao = new SimpleCourseIdeaDAO();
+
+        get("/", (req, res) -> {
+            Map<String, String> model = new HashMap<>();
+            model.put("username", req.cookie("username"));
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/sign-in", (request, response) -> {
+            Map<String, String> model = new HashMap<>();
+            String username = request.queryParams("username");
+            response.cookie("username", username);
+            model.put("username", username);
+            return new ModelAndView(model, "sign-in.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
+}
