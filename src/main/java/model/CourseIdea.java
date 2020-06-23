eaf90372a99db3d821d06eda34e490aa58ee41b4
Bruno Santos <brunog86@gmail.com>
2020-06-23 17:06:50 -0300

    Accessing Specific Items

--- src/main/java/model/CourseIdea.java
+++ src/main/java/model/CourseIdea.java
@@ -1,12 +1,36 @@
 package model;
 
+import com.github.slugify.Slugify;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
 public class CourseIdea {
+    private String slug;
     private String title;
     private String creator;
+    private Set<String> voters;
 
     public CourseIdea(String title, String creator) {
+        voters = new HashSet<>();
         this.title = title;
         this.creator = creator;
+        try {
+            Slugify slugify = new Slugify();
+            slug = slugify.slugify(title);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    public boolean addVoter(String voterUserName) {
+        return voters.add(voterUserName);
+    }
+
+    public int getVoteCount() {
+        return voters.size();
     }
 
     @Override
@@ -34,4 +58,9 @@ public class CourseIdea {
     public String getCreator() {
         return creator;
     }
+
+    public String getSlug() {
+        return slug;
+    }
 }
+
